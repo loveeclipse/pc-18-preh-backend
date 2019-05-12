3d94adca6a65940cc1dbe3ea0780fae72237b58e
Chiara <chiara.volonnino@studio.unibo.it>
2019-05-12 11:20:43 +0200

    Init get/eventId/oc-call.

--- vt-service/src/main/java/service/VtVerticle.java
+++ vt-service/src/main/java/service/VtVerticle.java
@@ -14,22 +14,26 @@ import java.util.UUID;
 
 public class VtVerticle {
 
-    // TODO: istanziarlo in una classe in modo da poterlo riusare in tutte le classi del package
     private static final Logger log = LoggerFactory.getLogger(VtVerticle.class);
 
+    private static final String DB_DOCUMENT_NAME = "events";
     // TODO: guadare confgurazione file e import
     private static final JsonObject MONGODB_CONFIGURATION = new JsonObject()
             .put("connection_string", "mongodb://loveeclipse:PC-preh2019@ds149676.mlab.com:49676/heroku_jw7pjmcr");
+
+
     private static Vertx vertx;
+
     static void setVertx(Vertx vertx){
         VtVerticle.vertx = vertx;
     }
 
     static void handlerGetAllEvent(RoutingContext routingContext) {
-        log.info("Get all tracking details by event-id and (eventually) mission-id");
+        log.info("Get all tracking details by event-id");
         HttpServerResponse response = getResponse(routingContext);
         String eventId = getRequest(routingContext).getParam("eventId");
        try {
+           // TODO: crea un udd partendo da una stringa... ma perchè qui?
            UUID.fromString(eventId);
            if (eventId.isEmpty()) {
                // TODO: chiedere con altri, se concordi da aggiungere in swagger.
@@ -37,7 +41,7 @@ public class VtVerticle {
            } else {
                JsonObject entries = new JsonObject().put("_id", eventId);
                MongoClient.createNonShared(vertx, MONGODB_CONFIGURATION)
-                       .find("events", entries, result -> {
+                       .find(DB_DOCUMENT_NAME, entries, result -> {
                            log.info("Mongodb connected!");
                            if(result.succeeded()){
                                try{
@@ -58,9 +62,15 @@ public class VtVerticle {
            log.info("BAD REQUEST");
            sendResponse(HttpResponseStatus.BAD_REQUEST, response);
        }
-
-
     }
+
+    static void handlerPostOcCall(RoutingContext routingContext) {
+        log.info("Get oc-call tracking details by event-id and (eventually) mission-id");
+        HttpServerResponse response = getResponse(routingContext);
+        String eventId = getRequest(routingContext).getParam("eventId");
+        String missionId = getRequest(routingContext).getParam("missionId");
+    }
+
 
     private static void sendResponse(HttpResponseStatus statusCode, HttpServerResponse response) {
         response.setStatusCode(statusCode.code()).end();
