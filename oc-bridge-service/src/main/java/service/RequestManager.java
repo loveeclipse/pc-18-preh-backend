268d7ddc6b07ff9f9f48f9238e3c26cd07776d86
Daniele Schiavi <schiavi91@gmail.com>
2019-05-14 19:03:32 +0200

    Update requests using body instead header

--- oc-bridge-service/src/main/java/service/RequestManager.java
+++ oc-bridge-service/src/main/java/service/RequestManager.java
@@ -3,6 +3,7 @@ package service;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.RoutingContext;
@@ -35,9 +36,9 @@ class RequestManager {
         MongoClient.createNonShared(vertx, config).insert("events", document, result -> {
             if (result.succeeded()) {
                 response
-                        .putHeader("eventId", uuid)
-                        .setStatusCode(200)
-                        .end();
+                        .putHeader("Content-Type", "application/json")
+                        .setStatusCode(201)
+                        .end(Json.encodePrettily(document));
             } else if (isDuplicateKey(result.cause().getMessage())) {
                 handleCreateEvent(routingContext);
             }
@@ -56,17 +57,17 @@ class RequestManager {
         String eventId = routingContext.request().getParam("eventId");
         try{
             UUID.fromString(eventId);
-            JsonObject queryResult = new JsonObject().put("_id", eventId);
-            MongoClient.createNonShared(vertx, config).find("events", queryResult, result -> {
+            JsonObject query = new JsonObject().put("_id", eventId);
+            MongoClient.createNonShared(vertx, config).find("events", query, result -> {
                 if(result.succeeded()){
                     try {
                         JsonObject resultJson = result.result().get(0);
                         resultJson.remove("_id");
                         if (resultJson.size() > 0)
                             response
-                                    .putHeader("eventInformation", resultJson.toString())
+                                    .putHeader("Content-Type", "application/json")
                                     .setStatusCode(200)
-                                    .end();
+                                    .end(Json.encodePrettily(resultJson));
                         else
                             response.setStatusCode(204).end();
                     } catch (IndexOutOfBoundsException ex) {
@@ -83,39 +84,18 @@ class RequestManager {
 
     static void handleUpdateEvent(RoutingContext routingContext){
         HttpServerResponse response = routingContext.response();
-        MultiMap params = routingContext.request().params();
-        JsonObject query = new JsonObject().put("_id", params.get("eventId"));
-        params.remove("eventId");
-        JsonObject document = new JsonObject();
-        try {
-            params.forEach(entry -> {
-                if (entry.getKey().equals("secondary")) {
-                    document.put(entry.getKey(), parseBoolean(entry.getValue()));
-                } else
-                    document.put(entry.getKey(), entry.getValue());
-            });
-            JsonObject update = new JsonObject().put("$set", document);
-            MongoClient.createNonShared(vertx, config).updateCollection("events", query, update, res -> {
-                if (res.succeeded()) {
-                    if (res.result().getDocModified() == 0)
-                        response.setStatusCode(400).end();
-                    else
-                        response.setStatusCode(200).end();
-                } else {
-                    response.setStatusCode(500).end();
-                }
-            });
-        } catch (IllegalArgumentException ex) {
-            response.setStatusCode(400).end();
-        }
-    }
-
-    private static boolean parseBoolean(String string) throws IllegalArgumentException {
-        if (string.equalsIgnoreCase("true"))
-            return true;
-        else if (string.equalsIgnoreCase("false"))
-            return false;
-        else
-            throw new IllegalArgumentException();
+        JsonObject body = routingContext.getBodyAsJson();
+        JsonObject query = new JsonObject().put("_id", routingContext.request().getParam("eventId"));
+        JsonObject update = new JsonObject().put("$set", body);
+        MongoClient.createNonShared(vertx, config).updateCollection("events", query, update, res -> {
+            if (res.succeeded()) {
+                if (res.result().getDocModified() == 0)
+                    response.setStatusCode(400).end();
+                else
+                    response.setStatusCode(204).end();
+            } else {
+                response.setStatusCode(500).end();
+            }
+        });
     }
 }
