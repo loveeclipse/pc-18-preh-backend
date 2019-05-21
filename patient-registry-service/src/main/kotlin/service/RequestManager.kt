598d78f307de40190c9e5f7f875219f5b2d5a606
Nicola Atti <nicola.atti@studio.unibo.it>
2019-05-21 23:32:41 +0200

    Initial service frame

--- patient-registry-service/src/main/kotlin/service/RequestManager.kt
+++ patient-registry-service/src/main/kotlin/service/RequestManager.kt
@@ -1,4 +1,35 @@
 package service
 
+import io.netty.handler.codec.http.HttpResponseStatus
+
+import io.netty.handler.codec.http.HttpResponseStatus.*
+import io.vertx.core.Vertx
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.mongo.MongoClient
+import io.vertx.ext.web.RoutingContext
+
+import java.util.UUID
+
 object RequestManager {
+    private var vertx: Vertx? = null
+    private val CONFIG = JsonObject().put("connection_string", "mongodb://loveeclipse:PC-preh2019@ds149676.mlab.com:49676/heroku_jw7pjmcr")
+
+    fun initializeRequestManager(vertx: Vertx) {
+        RequestManager.vertx = vertx
+    }
+
+    fun handleNewPatient(routingContext: RoutingContext) {
+
+    }
+
+    fun handleGetPatientData(routingContext: RoutingContext){
+
+    }
+
+    fun handleUpdatePatientData(routingContext: RoutingContext){
+
+    }
+
+
 }
